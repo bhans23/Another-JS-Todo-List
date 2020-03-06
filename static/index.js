6fcd40db96c4aceaf07abbc756657aa94e25ac7e
Ben <bhans23@gmail.com>
2020-03-06 11:51:31 -0700

    Fixed innerText issue

--- static/index.js
+++ static/index.js
@@ -1,11 +1,13 @@
 window.onload = () => {
-  alert("Hello world!");
+  const item1 = document.createElement("li");
+  const item2 = document.createElement("li");
+  const item3 = document.createElement("li");
+
+  item1.innerText = "take garbage out";
+  item2.innerText = "Play video game";
+  item3.innerText = "Take over the world";
+
+  const listItems = document.createElement("ul");
+  listItems.append(item1, item2, item3);
+  const vanillaList = document.getElementById("list").appendChild(listItems);
 };
-
-const item1 = (document.createElement("li").innerText = "take garbage out");
-const item2 = (document.createElement("li").innerText = "Play video game");
-const item3 = (document.createElement("li").innerText = "Take over the world");
-
-const listItems = document.createElement("ul");
-listItems.append(item1,item2,item3);
-const vanillaList = document.getElementById("list").appendChild(listItems);
