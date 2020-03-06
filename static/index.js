ed14e4f65ca72eae79c668de6bb4250ae91822db
Ben <bhans23@gmail.com>
2020-03-06 10:45:59 -0700

    Started adding JS list items

--- static/index.js
+++ static/index.js
@@ -1,5 +1,11 @@
 window.onload = () => {
-    alert("Hello world!");
-  };
+  alert("Hello world!");
+};
 
-  
+const item1 = (document.createElement("li").innerText = "take garbage out");
+const item2 = (document.createElement("li").innerText = "Play video game");
+const item3 = (document.createElement("li").innerText = "Take over the world");
+
+const listItems = document.createElement("ul");
+listItems.append(item1,item2,item3);
+const vanillaList = document.getElementById("list").appendChild(listItems);
