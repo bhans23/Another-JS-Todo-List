a0c04db6f84302d58a7f7fd50ffab7c819e9748a
Ben <bhans23@gmail.com>
2020-03-10 09:40:54 -0600

    adding lis and uls into the dom

--- static/index.js
+++ static/index.js
@@ -1,13 +1,34 @@
 window.onload = () => {
-  const item1 = document.createElement("li");
-  const item2 = document.createElement("li");
-  const item3 = document.createElement("li");
+  // const item1 = document.createElement("li");
+  // const item2 = document.createElement("li");
+  // const item3 = document.createElement("li");
 
-  item1.innerText = "take garbage out";
-  item2.innerText = "Play video game";
-  item3.innerText = "Take over the world";
+  // item1.innerText = "take garbage out";
+  // item2.innerText = "Play video game";
+  // item3.innerText = "Take over the world";
 
-  const listItems = document.createElement("ul");
-  listItems.append(item1, item2, item3);
-  const vanillaList = document.getElementById("list").appendChild(listItems);
+  // listItems.append(item1, item2, item3);
+  // const vanillaList = document.getElementById("list").appendChild(listItems);
+
+  const items = [
+    { title: "take garbage out", checked: true },
+    { title: "Play video games", checked: false },
+    { title: "Take over the world", checked: true }
+  ];
+
+  const todoList = (todoItem) => {
+    const todoListItems = document.createElement("ul").appendChild(todoItem);
+      document.getElementById("list").appendChild(todoListItems);
+  }
+
+  const todoItem = items => {
+    const itemsMapped = items.title.map.document.createElement("li");
+    const itemsMappedChecked = itemsMapped.checked.document.createElement(
+      "INPUT"
+    );
+    itemsMappedChecked.setAttribute("type", "checkbox");
+  };
+
+  document.todoList(todoItem)
+  
 };
